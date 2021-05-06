6d03914d60201313152262ea5faaffb2aca3b529
Santo Khan <santok54@gmail.com>
2021-05-06 23:55:27 +0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UI8 APP</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <style type="text/css">
+    <?php
+    /** require all sytles */
+    require 'bootstrap-5.0.0-beta2-dist/css/bootstrap.min.css';
+    require 'css/app.css';
+    require 'css/appcontainer.css';
+    require 'css/keyframe.css';
+    require 'css/navbar.css';
+    require 'css/topbar.css';
+    ?>
+    </style>
+</head>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php require 'header.php'; ?>
+
+<body>
+    <div class="app">
+        <nav class="nav-bar">
+            <?php include 'navbar/navbar.php' ?>
+        </nav>
+
+        <div class="app-container">
+            <?php include 'appcontainer/appcontainer.php' ?>
+        </div>
+    </div>
+
+    <script>
+    <?php require 'bootstrap-5.0.0-beta2-dist/js/bootstrap.bundle.min.js';?>
+    </script>
+</body>
+
+</html>
